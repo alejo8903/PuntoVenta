12a7b8dad4b5d3cd0f3810d82f8420b17b1e21a7
Alejandro Mira <alejo890327@gmail.com>
2023-07-26 19:31:38 -0500

    Formulario login

---
+++ src/main/java/com/intesoft/puntoventa/PuntoVenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.intesoft.puntoventa;
+
+/**
+ *
+ * @author alejo
+ */
+public class PuntoVenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
